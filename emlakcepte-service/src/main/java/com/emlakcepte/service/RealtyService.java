65c41a46697a25e5c33fef489d636402655d40f9
elaadurdu <96113005+elaadurdu@users.noreply.github.com>
2022-12-18 22:21:50 +0300

    Update RealtyService.java

--- emlakcepte-service/src/main/java/com/emlakcepte/service/RealtyService.java
+++ emlakcepte-service/src/main/java/com/emlakcepte/service/RealtyService.java
@@ -1,62 +1,94 @@
-package com.emlakcepte.service;
+package emlakcepte.service;
+import emlakcepte.model.*;
+import emlakcepte.dao.RealtyDao;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.emlakcepte.model.Realty;
-import com.emlakcepte.model.User;
-import com.emlakcepte.model.enums.RealtyType;
-import com.emlakcepte.model.enums.UserType;
-import com.emlakcepte.repository.RealtyRepository;
+import emlakcepte.dao.RealtyDao;
+import emlakcepte.model.Realty;
+import emlakcepte.model.RealtyType;
+import emlakcepte.model.Types;
+import emlakcepte.model.User;
+import emlakcepte.model.UserType;
+
+
+
 
 @Service
+
 public class RealtyService {
-
-	private RealtyRepository realtyDao = new RealtyRepository();
-
-	//@Autowired // injection
+	
+	private RealtyDao realtyDao = new RealtyDao();
+	
+	@Autowired //injection
 	private UserService userService;
-
-	public void createRealty(Realty realty) {
-
-		// userService.printAllUser();
-
-		if (UserType.INDIVIDUAL.equals(realty.getUser().getType())) {
+	
+	public void createRealty(Realty realty) {	
+		
+		//userService.printAllUser();
+		
+		if (UserType.INDIVIDUAL.equals(realty.getUser().getType()) && Types.KONUT.equals(realty.getTypes()) ) {
 			// en fazla 5 ilan girebilir.
-
-			System.out.println("Bireysel kullanıclar en fazla 5 ilan girebilir.");
+		if(realty.getUser().getRealtyList().size() <3  ) {
+			//System.out.println(realty.getUser().getRealtyList().size()+"sizeee");
+			
+			realtyDao.saveRealty(realty);
+			System.out.println("createRealty :: " + realty.getTitle() + realty.getTypes()+realty.getUser().getName());
 		}
+			
+			
+		}
+		else {
+			
+		
+		System.out.println("Bireysel kullanıclar KONUT tipinde ve en fazla 3 ilan girebilir.");
 
-		realtyDao.saveRealty(realty);
-		System.out.println("createRealty :: " + realty.getTitle());
+		}
+			
+		
 	}
-
-	public List<Realty> getAll() {
+	
+	public List<Realty> getAll(){
 		return realtyDao.findAll();
 	}
-
+	
 	public void printAll(List<Realty> realtList) {
 		realtList.forEach(realty -> System.out.println(realty));
 	}
 
 	public void getAllByProvince(String province) {
-
-		getAll().stream().filter(realty -> realty.getProvince().equals(province))
-				// .count();
-				.forEach(realty -> System.out.println(realty));
-
+		
+		getAll().stream()
+		.filter(realty -> realty.getProvince().equals(province))
+		//.count();
+		.forEach(realty -> System.out.println(realty));
+		
 	}
-
-	public List<Realty> getAllByUserName(User user) {
-		return getAll().stream().filter(realty -> realty.getUser().getMail().equals(user.getMail())).toList();
+	public void getAllByProvinceDistrict(String province,String district) {
+		
+		getAll().stream()
+		.filter(realty -> realty.getProvince().equals(province))
+		.filter(realty->realty.getDistrict().equals(district))
+		//.count();
+		.forEach(realty -> System.out.println(realty));
+		
+	}
+	
+	public List<Realty> getAllByUserName(User user){	
+		return getAll().stream()
+		.filter(realty -> realty.getUser().getMail().equals(user.getMail()))
+		.toList();		
 	}
 
 	public List<Realty> getActiveRealtyByUserName(User user) {
-
-		return getAll().stream().filter(realty -> realty.getUser().getName().equals(user.getName()))
-				.filter(realty -> RealtyType.ACTIVE.equals(realty.getStatus())).toList();
+		
+		return getAll().stream()
+		.filter(realty -> realty.getUser().getName().equals(user.getName()))
+		.filter(realty -> RealtyType.ACTIVE.equals(realty.getStatus()))
+		.toList();
 
 	}
 
